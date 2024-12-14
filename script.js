5c01818b3dc0d1170497489476713c1ea7162326
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-14 14:52:20 +0200

    added javascript file

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from("#header", { duration: 1, y: "-100%", ease: "bounce" });
+gsap.from("#icondiv,#bar", { duration: 2, opacity: 0, delay: 1 });
+gsap.from("#sss", { duration: 2.5, y: "-100%", ease: "bounce" });
+gsap.from("#text,#footer", { duration: 3, opacity: 0, delay: 2 });
